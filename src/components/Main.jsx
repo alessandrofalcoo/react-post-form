62f0f78b279242fef22902dd914fafa5acd4dd2e
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-31 16:06:12 +0200

    add form submission handling and checkbox state management in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,41 +6,67 @@ export default function Main() {
     const [postDescription, setPostDescription] = useState('')
     const [checkbox, setCheckbox] = useState(false)
 
+    fetch('https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts')
+        .then(res => res.json(
+
+        ))
+        .then(data => {
+
+        });
+
+    function handleCheckBox(element) {
+        if (element) {
+            setCheckbox('Available');
+
+        } else if (element === false) {
+            setCheckbox('Unavailable');
+
+        }
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault()
+        console.log('button clicked');
+
+    }
+
+
     return (
         <>
             <div className="container">
                 <form>
-                    <div class="mb-3">
-                        <label for="" class="form-label"></label>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label"></label>
                         <input
                             type="text"
-                            class="form-control"
+                            className="form-control"
                             name=""
                             id=""
                             aria-describedby="helpId"
                             placeholder="Inserisci il nome dell'autore..."
                         />
                     </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label"></label>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label"></label>
                         <input
                             type="text"
-                            class="form-control"
+                            className="form-control"
                             name=""
                             id=""
                             aria-describedby="helpId"
                             placeholder="Inserisci il titolo del post..."
                         />
                     </div>
-                    <div class="mb-3">
-                        <label for="body" class="form-label"></label>
-                        <textarea class="form-control" name="body" id="body" rows="6"></textarea>
+                    <div className="mb-3">
+                        <label htmlFor="body" className="form-label"></label>
+                        <textarea className="form-control" name="body" id="body" rows="6"></textarea>
                     </div>
 
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="checkbox" id="checkbox" />
-                        <label class="form-check-label" for="checkbox" ></label>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="checkbox" onChange={handleCheckBox} />
+                        <label className="form-check-label" htmlFor="checkbox" >{checkbox}</label>
                     </div>
+                    <button type="submit" className="btn btn-primary mt-3" onChange={handleFormSubmit} value={e => e.target.value}>Submit</button>
 
                 </form>
 
